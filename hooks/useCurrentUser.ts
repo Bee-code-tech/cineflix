c7639e67e50cfe3ceee69c5f4b76ba5aa8672857
Al-Ameen Babawale <60514061+Bee-code-tech@users.noreply.github.com>
2024-06-26 23:32:23 +0100

    Update useCurrentUser.ts

--- hooks/useCurrentUser.ts
+++ hooks/useCurrentUser.ts
@@ -1,7 +1,7 @@
 import useSwr from 'swr';
 import fetcher from '../lib/fetcher';
 
-const useCurrentUser = ()=> {
+const useCurrentUser = )=> {
     const {data, error, isLoading, mutate} = useSwr('/api/current', fetcher);
 
     return{
